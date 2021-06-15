bf81f0a36cadd52d0bdfb52be25e9666886dafe7
Vicavio <cuenta.vica@gmail.com>
2021-06-15 14:51:26 +0200

    Commit changes

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
-const newJoke = document.getElementById('newJoke');
+const newJoke = document.getElementById("newJoke");
 const url = "https://icanhazdadjoke.com/";
 
 
 const anotherJoke = async () => {
-	const urlToFetch = `${url}`;
-        try {
-          const response = await fetch(urlToFetch);
-          if(response.ok){
-            console.log(response);
-            return response;
-            const jsonResponse = await response.json();
-            console.log(jsonResponse);
-            document.getElementById('joke').innerHTML = jsonResponse.response;
-          }
-        } catch(error) {
-          console.log(error);
-      }
+
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Accept: "application/json",
+      },
+    });
+
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      console.log(jsonResponse);
+      document.getElementById("joke").innerHTML = jsonResponse.joke;
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
-newJoke.addEventListener('click', anotherJoke);
+
+newJoke.addEventListener("click", anotherJoke);
+
+
